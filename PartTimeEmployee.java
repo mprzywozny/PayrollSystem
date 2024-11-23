1a2a2fec897bb99cb51eab3c08532fc20db3b008
c-mol <ciaramolloy19@gmail.com>
2024-11-23 17:03:12 +0000

    Update PartTimeEmployee.java

--- PartTimeEmployee.java
+++ PartTimeEmployee.java
@@ -1,12 +1,14 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.Scanner;
 
-public class PartTimeEmployee extends employee {
+public class PartTimeEmployee extends employee
+{
     private double hourlyRate;
     private double hoursWorked;
 
-    public PartTimeEmployee(String jobCategory, String jobTitle, int scalePoint, double hourlyRate) throws FileNotFoundException {
-        super(jobCategory, jobTitle, scalePoint);
+    public PartTimeEmployee(String name,String jobCategory, String jobTitle, int scalePoint, double hourlyRate) throws FileNotFoundException {
+        super(name,jobCategory, jobTitle, scalePoint);
         this.hourlyRate = hourlyRate;
         this.hoursWorked = 0; //hours reset each month hence 0, new hours for current period are to be added using setter method below
     }
@@ -31,11 +33,13 @@ public class PartTimeEmployee extends employee {
         this.hoursWorked += hoursWorked + hours;
         return hoursWorked;
     }
-
+    public void setsalary() {
+    	this.manualsetSalary(grossPay());
+    }
 
    //Calculate gross pay given hours worked and hourly rate
     public double grossPay() {
-        double grossPay = hoursWorked + hourlyRate;
+        double grossPay = hoursWorked * hourlyRate;
         return grossPay;
     }
 
@@ -44,4 +48,79 @@ public class PartTimeEmployee extends employee {
         this.hoursWorked = 0;
         System.out.printf("Pay claim submitted successfully. Gross pay: %.2f", grossPay());
     }
+    @Override
+	public void moveupincatagory() throws FileNotFoundException {
+		String current=this.getJobtitle();
+		line = new Scanner(new File("../group/src/payscale for partime.csv"));
+		boolean done=false;
+		String change=current;
+		line.nextLine();
+		while(!done&&line.hasNext()) {
+			String input=line.nextLine();
+			if(input.equals(this.getJobcatagory())) {
+				//in the right catagory
+				while(line.hasNext()&&!done) {
+					input=line.nextLine();
+					String[] parts=input.split(",");
+					if( current.equals(parts[0]) )
+					{
+					done=true;
+					}
+					else{change=parts[0];}
+				
+				}
+				}
+			
+		}
+		if(change!=current) {
+		this.setjobtitle(change);
+		this.setscalepoint(1);
+		this.setsalary();
+		}
+		else {System.out.print("At the top\n");}
+		
+	}
+	public int gettopPayscale() throws FileNotFoundException {
+		line = new Scanner(new File("../group/src/payscale for partime.csv"));
+		
+		boolean done=false;
+		//int topPayscale;
+		line.nextLine();
+		while(line.hasNext())
+		{
+			String input=line.nextLine();
+			
+			if(input.equals(this.getJobcatagory())) {
+				
+				while(line.hasNext()&&!done) {
+					input=line.nextLine();
+					String[] parts=input.split(",");
+					
+					if( this.getJobtitle().equals(parts[0]) )
+					{
+					input=line.nextLine();
+					
+					return Integer.parseInt(parts[1]);
+					}
+				
+				}
+				}
+		}
+	
+		return -1;
+	}
+	
+	public void moveupsalarypoint() throws FileNotFoundException {//check if theycan move up
+		if(this.gettopPayscale()>this.getScalepoint()) {
+			this.setscalepoint(this.getScalepoint()+1);
+			this.setsalary();
+			;}
+		else {System.out.println("Top of payscale\n");}
+	}
+    @Override
+    public String toString() {
+    	return this.getName()+","+this.getJobcatagory()+","+this.getJobtitle()+","+this.getScalepoint()+","+this.hourlyRate+","+this.hoursWorked ;
+		
+    }
+  
 }
