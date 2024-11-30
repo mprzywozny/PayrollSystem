75edb3650962d50cba4e66862f8c45577906261e
Amilkamija <166129881+Amilkamija@users.noreply.github.com>
2024-11-30 15:17:05 +0000

    Update User.java

--- User.java
+++ User.java
@@ -5,26 +5,26 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Scanner;
 /**class that writes users to the csv file and puts them in the right format*/
-public class user {
+public class User {
 private String id;//input
-private String usertype;//set
+private String userType;//set
 private Scanner s;
 PrintWriter writer;
 
 
-user(String id,String u) throws IOException{
+User(String id,String u) throws IOException{
 	u.toLowerCase();
 	String x=id+","+u;
-	writetolistofusers(x);
+	writeToListOfUsers(x);
 }
 /**
  * writes the user to a list of users
  * @param x user and their usertype to be written into users.csv
  * @throws IOException if users.csv not found
  * */
-public void writetolistofusers(String x) throws IOException {
+public void writeToListOfUsers(String x) throws IOException {
 	//find a new line
-	writer=new PrintWriter(new FileWriter("../group/src/users.csv",true));
+	writer = new PrintWriter(new FileWriter("src/Users.csv",true));
 	
 	writer.println(x);
 	writer.close();
